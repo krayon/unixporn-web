d56503547f5738b65953a9085dc9edf0d3f009ad
Oscar Holst <oscar.holst@linux.com>
2016-10-24 05:43:55 +0200

    Changed external link
    Changed github repo link

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 
 <footer class="footer">
 <div class="container">
-<p class="footerish">&copy; 2014-<?php echo date("Y") ?> nixplorer.org | <a href="https://github.com/OscarHolst/nixplorer" target="_BLANK"><img src="img/github.png"></a> | <a href="tos.php">ToS</a> | <a href="mailto:webmaster@nixplorer.org">Contact</a></p>
+<p class="footerish">&copy; 2014-<?php echo date("Y") ?> nixplorer.org | <a href="https://github.com/OscarHolst/nixplorer-web/" target="_BLANK"><img src="img/github.png"></a> | <a href="tos.php">ToS</a> | <a href="mailto:webmaster@nixplorer.org">Contact</a></p>
 </div>
 </footer>
 
