e0d3071545f4d0982b08915ad3bebb302f8acf66
Oscar Holst <OscarHolst@users.noreply.github.com>
2016-12-13 04:37:46 +0100

    Added RSS feed

---
+++ rss.php
@@ -0,0 +1,27 @@
+<?php
+include_once('inc/db.php'); 
+
+$stmt = 'SELECT * FROM uploads ORDER BY id DESC LIMIT 10';
+$query = $conn->prepare($stmt);
+$query->execute();
+$posts = $query->fetchAll();
+
+$data = '<?xml version="1.0" encoding="UTF-8"?>';
+$data .= '<rss version="2.0">';
+$data .= '<channel>';
+$data .= '<title>Nixplorer</title>';
+$data .= '<link>https://www.nixplorer.org</link>';
+$data .= '<description>Free GNU/Linux image hosting and screenshot sharing</description>';
+    foreach ($posts as $row) {
+    $data .= '<item>';
+    $data .= '<title>'.$row['imgText'].'</title>';
+    $data .= '<link>https://nixplorer.org/i/'.$row['imgName'].'</link>';
+    $data .= '<description>'.date("Y-m-d",$row['imgDate']).'</description>';
+    $data .= '</item>';
+    }
+$data .= '</channel>';
+$data .= '</rss>';
+
+header('Content-Type: application/xml');
+echo $data;
+?>
