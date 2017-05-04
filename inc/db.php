f0d7ff0ea6426383eae00fab42a68b745756d046
Krayon <krayon.git@qdnx.org>
2017-05-04 10:35:14 +1000

    Added mysql_like_wildcard_{un}escape functions
    PDO MySQL queries don't have '%' or '_' escaped since they are not
    special characters in SQL, only in a LIKE comparison.

--- inc/db.php
+++ inc/db.php
@@ -2,7 +2,28 @@
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
 
-date_default_timezone_set('Europe/Stockholm');
+//date_default_timezone_set('Europe/Stockholm');
+
+// PDO MySQL queries don't have '%' or '_' escaped since they are not special
+// characters in SQL, only in a LIKE comparison. Thus we have our own
+// functions.
+// {
+function mysql_like_wildcard_escape($str) {
+    return str_replace(
+         array(  '\\',   '_',   '%')
+        ,array('\\\\', '\\_', '\\%')
+        ,$str
+    );
+}
+
+function mysql_like_wildcard_unescape($str) {
+    return str_replace(
+         array('\\\\', '\\_', '\\%')
+        ,array(  '\\',   '_',   '%')
+        ,$str
+    );
+}
+// }
 
 try {
 $conn = new PDO('mysql:host=localhost;dbname=DBNAME;charset=utf8','USERNAME','PASSWORD');
