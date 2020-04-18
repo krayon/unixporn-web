88717e56a7f9adf5ef728b4eeaee797515760606
Krayon <krayon.git@qdnx.org>
2020-04-19 06:29:47 +1000

    Add leading wildcard, min 3 chars
    Signed-off-by: Krayon <krayon.git@qdnx.org>

--- inc/explore.php
+++ inc/explore.php
@@ -3,33 +3,33 @@
 // Find total number of rows in table
 $rowCnt = $conn->query("SELECT COUNT(*) FROM uploads")->fetchColumn();
 $total_rows = $rowCnt;
- 
+
 // Set rows per page
 $rows_per_page = 18;
 
 // Calculate total number of pages
 $total_pages = ceil($total_rows / $rows_per_page);
- 
+
 // Get current page
 $current_page = (isset($_GET['p']) && $_GET['p'] > 0) ? (int) $_GET['p'] : 1;
- 
+
 // If current page is greater than last page, set it to last.
 if ($current_page > $total_pages)
     $current_page = $total_pages;
- 
+
 // Set starting post
 $offset = ($current_page - 1) * $rows_per_page;
- 
+
 // Get rows from database
 $stmt = $conn->query("SELECT * FROM uploads ORDER BY imgDate DESC LIMIT $offset, $rows_per_page");
- 
+
 // Print rows
   if (!isset($_POST['query'])) {
         while($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
         $id = $row['id'];
         $imgName = $row['imgName'];
-        $imgText = $row['imgText']; 
+        $imgText = $row['imgText'];
         $imgDate = $row['imgDate'];
         $imgThumb = $row['imgThumb'];
 
@@ -60,7 +60,7 @@ else
         <span aria-hidden="true">&laquo;</span>
       </a>
     </li>';
- 
+
 // Create page links, based on chosen range.
 for ($i = $current_page - $range; $i < ($current_page + $range) + 1; $i++)
 {
@@ -70,14 +70,14 @@ for ($i = $current_page - $range; $i < ($current_page + $range) + 1; $i++)
       else
          echo '<li><a href="?p='.$i.'">'.$i.'</a></li>';
 }
- 
+
  // Create navigation link for next and last page.
 if ($current_page != $total_pages)
 {
     $next = $current_page + 1;
     if (($current_page + $range) < $total_pages)
         echo '<li><a href="?p='.$total_pages.'">'.$total_pages.'</a></li> ';
- 
+
     echo '<li>
       <a href="?p='.$next.'" aria-label="Next">
         <span aria-hidden="true">&raquo;</span>
@@ -91,31 +91,34 @@ else
       </a>
     </li>';
   echo '</ul></nav>';
-        
+
   }
     if (isset($_POST['query'])) {
-        // Clean query string for HTML (hquery) and SQL (squery)
-        $hquery = htmlspecialchars($_POST['query']);
-        $squery = mysql_like_wildcard_escape($_POST['query']);
+        if (strlen($_POST['query']) < 3) {
+            echo '<p>Query needs to be at least 3 characters long</p>';
+        } else {
+            // Clean query string for HTML (hquery) and SQL (squery)
+            $hquery = htmlspecialchars($_POST['query']);
+            $squery = mysql_like_wildcard_escape($_POST['query']);
 
-        // Prepare and execute query
-        $stmt = $conn->prepare("SELECT * FROM uploads WHERE imgText LIKE ? ORDER BY imgDate DESC LIMIT 24");
-        $stmt->execute(array($squery . '%'));
+            // Prepare and execute query
+            $stmt = $conn->prepare("SELECT * FROM uploads WHERE imgText LIKE ? ORDER BY imgDate DESC LIMIT 24");
+            $stmt->execute(array('%' . $squery . '%'));
 
-    if ($stmt->rowCount() > 0) {
-        echo '<h4>Results for: ' . $hquery . '</h4>';
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-         $id = $row['id'];
-         $imgName = $row['imgName'];
-         $imgText = $row['imgText']; 
-         $imgDate = $row['imgDate'];
-         $imgThumb = $row['imgThumb'];
+            if ($stmt->rowCount() > 0) {
+                echo '<h4>Results for: ' . $hquery . '</h4>';
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    $id = $row['id'];
+                    $imgName = $row['imgName'];
+                    $imgText = $row['imgText'];
+                    $imgDate = $row['imgDate'];
+                    $imgThumb = $row['imgThumb'];
 
-         echo '<a href="//' . $site_url . '/i/'.$imgName.'" data-title="'.$imgText.' | ' . $site_url . '/i/'.$imgName.'" data-lightbox="latest"><img src="//' . $site_url . '/i/'.$imgThumb.'" class="img-thumbnail" title="'.$imgText.'" alt="image"/></a>';
-}
-  } 
-    else {
-       echo '<p>No results for ' . $hquery . '...</p>';
+                    echo '<a href="//' . $site_url . '/i/'.$imgName.'" data-title="'.$imgText.' | ' . $site_url . '/i/'.$imgName.'" data-lightbox="latest"><img src="//' . $site_url . '/i/'.$imgThumb.'" class="img-thumbnail" title="'.$imgText.'" alt="image"/></a>';
+                }
+            } else {
+                echo '<p>No results for ' . $hquery . '...</p>';
+            }
+        }
     }
-  }
 ?>
