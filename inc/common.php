b8f1b1ca2635a27fe5a79f3519482952ab90f3b2
Krayon <krayon.git@qdnx.org>
2020-04-19 05:52:43 +1000

    Made notices configurable
    Signed-off-by: Krayon <krayon.git@qdnx.org>

--- inc/common.php
+++ inc/common.php
@@ -3,4 +3,8 @@ $site_name  = 'Nixplorer';
 $site_url   = 'www.nixplorer.org';
 $src_url   =  'https://github.com/OscarHolst/nixplorer-web/';
 $site_email = 'webmaster@nixplorer.org';
+
+$site_notices = [
+    '<strong>Notice!</strong> We have moved from www.unixporn.org to www.nixplorer.org'
+];
 ?>
--- index.php
+++ index.php
@@ -6,9 +6,18 @@
 <input name="query" type="search" class="form-control form-ctrl-search" size="100%" placeholder="Search for screenshots..." required>
 </form> </div>
 
+<?php
+if (isset($site_notices)) {
+  foreach ($site_notices as $notice) {
+?>
 <div class="alert alert-headsup" role="alert">
-<span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
-<strong>Notice!</strong> We have moved from www.unixporn.org to www.nixplorer.org</div>
+  <span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
+  <?php echo $notice; ?>
+</div>
+<?php
+  }
+}
+?>
 
 <div class="row">
 <div class="col-md-12">
